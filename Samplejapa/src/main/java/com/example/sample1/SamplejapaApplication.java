3a52724c041d65568c0a007328f524d893bfa014
Hemanthmca <hemanthgr97@gmail,com>
2022-07-13 10:44:02 +0530

    project added

---
+++ Samplejapa/src/main/java/com/example/sample1/SamplejapaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplejapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplejapaApplication.class, args);
+	}
+
+}
